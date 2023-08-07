53e4652030f3fa101514433b2451b94476254d84
alxqkekrroqkfwk <fexxet@icloud.com>
2023-08-07 11:29:41 +0900

    init

---
+++ src/main/java/com/example/hybrid_kanbanboard/HybridKanbanBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.hybrid_kanbanboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybridKanbanBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HybridKanbanBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hybrid_kanbanboard/HybridKanbanBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hybrid_kanbanboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridKanbanBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
