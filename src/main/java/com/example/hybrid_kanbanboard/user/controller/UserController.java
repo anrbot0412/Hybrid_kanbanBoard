28dac5a871fe6d0bbee637d42392bb392a385463
Jongwoo Choi <muvnelik@naver.com>
2023-08-07 19:48:53 +0900

    [update] User관련 URL 변경 api/login -> hybrid/login이런 식으로 api대신 hybrid로 변경

--- src/main/java/com/example/hybrid_kanbanboard/user/controller/UserController.java
+++ src/main/java/com/example/hybrid_kanbanboard/user/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/hybrid")
 public class UserController {
 
     private final UserService userService;
